923571cb03263a3d4df9a52f1f8ad2433be3542a
Aya1602 <aygerim.m1602@gmail.com>
2021-04-24 09:32:43 +0600

    Initial commit

---
+++ app/src/main/java/com/example/chapterthreeparttwo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.chapterthreeparttwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {}
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {}
+
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                TextView textView = (TextView) findViewById(R.id.textView);
+                textView.setText(s);
+            }
+        });
+    }
+}
